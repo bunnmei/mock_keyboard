b527fb261ff4e7667e024c281a2523ab88b938b6
bunnmei <kombinat.web@gmail.com>
2025-10-23 17:37:28 +0900

    key read

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,50 @@ async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
     let mut led: Output<'_> = Output::new(p.P0_15, Level::Low, OutputDrive::Standard);
 
-    let mut cols = [
-        Output::new(p.P0_29, Level::High, OutputDrive::Standard),
-        Output::new(p.P0_02, Level::High, OutputDrive::Standard),
-        Output::new(p.P1_15, Level::High, OutputDrive::Standard),
-        Output::new(p.P1_13, Level::High, OutputDrive::Standard),
-        Output::new(p.P1_11, Level::High, OutputDrive::Standard),
+   let mut cols = [
+        Output::new(p.P0_29, Level::Low, OutputDrive::Standard), // High -> Low
+        Output::new(p.P0_02, Level::Low, OutputDrive::Standard), // High -> Low
+        Output::new(p.P1_15, Level::Low, OutputDrive::Standard), // High -> Low
+        Output::new(p.P1_13, Level::Low, OutputDrive::Standard), // High -> Low
+        Output::new(p.P1_11, Level::Low, OutputDrive::Standard), // High -> Low
     ];
-
     let rows= [
-        Input::new(p.P1_04, Pull::Up),
-        Input::new(p.P1_06, Pull::Up),
-        // Input::new(p.P0_09, Pull::Up),
-        // Input::new(p.P0_10, Pull::Up),
+        Input::new(p.P1_04, Pull::Down), // Up -> Down
+        Input::new(p.P1_06, Pull::Down), // Up -> Down
+        // Input::new(p.P0_09, Pull::Down),
+        // Input::new(p.P0_10, Pull::Down),
     ];
 
+    // let mut col0: Output<'_> = Output::new(p.P0_29, Level::High, OutputDrive::Standard);
+
+    // let row0 = Input::new(p.P1_04, Pull::Up);
     led.set_high();
     Timer::after_millis(300).await;
 
     loop {
+        // col0.set_low();
+        // if row0.is_low() {
+        //     info!("row low")
+        // } else {
+        //     info!("row high")
+        // }
+
+        // Timer::after_millis(300).await;
         let mut state = [[false; 5]; 2];
 
         for(ci, col) in cols.iter_mut().enumerate() {
-            col.set_low();
+            col.set_high();
 
             Timer::after(Duration::from_micros(50)).await;
 
             for(ri, row) in rows.iter().enumerate() {
-                if row.is_low() {
+                if row.is_high() { // is_low() -> is_high()
                     state[ri][ci] = true;
+                    // info!("Key pressed: row={} col={}", ri, ci);
                 }
             }
 
-            col.set_high();
+            col.set_low();
         }
 
         for (ri, row) in state.iter().enumerate() {
@@ -60,6 +71,6 @@ async fn main(_spawner: Spawner) {
         
         // led.set_low();
         // Timer::after_millis(300).await;
-        info!("loop");
+        // info!("loop");
     }
 }
